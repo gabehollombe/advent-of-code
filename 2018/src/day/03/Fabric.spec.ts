322afdf011010b8f016e0e74b184ac053e0d86e2
Gabe Hollombe <gabe@avantbard.com>
2018-12-19 10:46:34 +0800

    Fix spec input for claim overlap

--- 2018/src/day/03/Fabric.spec.ts
+++ 2018/src/day/03/Fabric.spec.ts
@@ -7,8 +7,8 @@ describe('Tracking cells claimed', function() {
     it('counts the number of overlapping cells', () => {
         let fabric = new Fabric();
         fabric.addClaim(Claim.fromString('#1 @ 1,3: 4x4'))
-        fabric.addClaim(Claim.fromString('#2 @ 3, 1: 4x4'))
-        fabric.addClaim(Claim.fromString('#3 @ 5, 5: 2x2'))
+        fabric.addClaim(Claim.fromString('#2 @ 3,1: 4x4'))
+        fabric.addClaim(Claim.fromString('#3 @ 5,5: 2x2'))
 
         expect(fabric.overlapCount()).toEqual(4);
         
